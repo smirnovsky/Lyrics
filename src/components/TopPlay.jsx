b87709e16758a1f4f8f3653bf51df9af2191ebcb
smirnovsky <happy280190@gmail.com>
2023-01-26 14:11:14 +0300

    feat: add top charts

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -10,9 +10,27 @@ import 'swiper/css';
 import 'swiper/css/free-mode'
 import PlayPause from './PlayPause';
 
-const TopChartCard = ({ song, i }) => (
+const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    {song.title}
+    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img className="w-10 h-10 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+        </Link>
+      </div>
+    </div>
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClick}
+      handlePlay={handlePlayClick} 
+    />
   </div>
 )
 
@@ -32,7 +50,7 @@ const TopPlay = () => {
     dispatch(playPause(false))
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true))
   };
@@ -53,6 +71,10 @@ const TopPlay = () => {
               key={song.key}
               song={song}
               i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, i)}
             />
           ))}
         </div>
@@ -78,7 +100,7 @@ const TopPlay = () => {
               {topPlays?.map((song, i) => (
                 <SwiperSlide
                   key={song?.key}
-                  style={{ width: '25%', height: 'auto' }}
+                  style={{ width: '15%', height: 'auto' }}
                   className="shadow-lg rounded-full animate-slideright"
                 >
                   <Link to={`/artists/${song?.artists[0].adamid}`}>
