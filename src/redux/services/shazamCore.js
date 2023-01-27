adfd6a7b9557fc4f901117e1e9bd597b77acd4e1
smirnovsky <happy280190@gmail.com>
2023-01-27 16:43:16 +0300

    fix: fix error with 404 server response

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
  export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi ',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', 'f6c1e33860msh4b193527828127bp100cf9jsn8fcf1ff22f1d');
 
@@ -11,9 +11,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/world' }),
-        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
-        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+        getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+        getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
     }),
  });
 
@@ -21,4 +22,5 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
     useGetTopChartsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
  } = shazamCoreApi;
