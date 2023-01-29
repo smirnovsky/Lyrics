b306f093beb8af38cb978f5bca2bf15ea7d70786
smirnovsky <happy280190@gmail.com>
2023-01-29 16:54:41 +0300

    feat: add genre selector

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,6 +12,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
         getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
         getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
@@ -22,6 +23,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
  export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
